776faa0b842ee5b69f2e1b0a20f3b84de1dee5f0
Noob_Hacker <gauravjais4491@gmail.com>
2024-03-11 11:21:59 +0530

    add try catch to handle capcha

--- tests/contact.spec.js
+++ tests/contact.spec.js
@@ -5,8 +5,15 @@ import contactData from '../Data/contactData.json'
 customTest.beforeEach('should go to contact page', async ({ page, homePage }) => {
     await page.goto('/', { waitUntil: 'networkidle' })
     await homePage.goToContactPage()
+    await page.waitForLoadState('networkidle')
 })
-customTest('should create contact details', async ({ page, contactPage, securePageForContactPage }) => {
-    await contactPage.addContactDetails(contactData.name, contactData.email, contactData.phone, contactData.message)
-    expect((await securePageForContactPage.flashMessage.textContent())?.trim()).toBe(contactData.expectTextForSubmitContact)
+customTest('should create contact details', async ({ contactPage, securePageForContactPage }, testInfo) => {
+    try {
+        console.log(testInfo.title);
+        await contactPage.addContactDetails(contactData.name, contactData.email, contactData.phone, contactData.message)
+        expect((await securePageForContactPage.flashMessage.textContent())?.trim()).toBe(contactData.expectTextForSubmitContact)
+    } catch (error) {
+        await page.screenshots('./screenshots/' + testInfo.title + '.png')
+        console.log("Error occured during contact tests: " + error);
+    }
 })
