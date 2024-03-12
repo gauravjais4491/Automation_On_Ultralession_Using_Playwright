81c6b4103b4c8c63ee677a1ab6e0b800c2a51566
Noob_Hacker <gauravjais4491@gmail.com>
2024-03-12 13:10:57 +0530

    add testinfo for debugging

--- tests/FeatureTests/storePage.e2e.spec.js
+++ tests/FeatureTests/storePage.e2e.spec.js
@@ -30,6 +30,17 @@ customTest('should filter product according to product size @productSize', async
 
 })
 
+customTest('Verify Filtering Functionality Based on Product Availability @stock', async ({ storePage, securePageForStorePage }) => {
+    await storePage.filterProductAccordingToStock(testData.stock)
+    await securePageForStorePage.isFilteredAccordingToStockOrNot(testData.stock)
+
+})
+customTest('Verify Filtering Functionality Based on Product Availability @outOfStock', async ({ storePage, securePageForStorePage }) => {
+    await storePage.filterProductAccordingToOutOfStock(testData.outOfStock)
+    await securePageForStorePage.isFilteredAccordingToOutOfStockOrNot(testData.outOfStock)
+
+})
+
 
 
 
--- tests/e2e/e2e.spec.js
+++ tests/e2e/e2e.spec.js
@@ -2,34 +2,34 @@ import e2eData from '../../Data/e2eData.json'
 import { customTest } from '../../Fixtures/e2eFixture.js'
 
 for (const data of e2eData) {
-    customTest(`should place an order with product name ' ${data.productName} '`, async ({ homePage, storePage, page, productDetailsPage, viewCartPage, shoppingAddressPage, paymentDetailsPage }) => {
+    customTest(`should place an order with product name ' ${data.productName} '`, async ({ homePage, storePage, page, productDetailsPage, viewCartPage, shoppingAddressPage, paymentDetailsPage },testInfo) => {
 
-        await customTest.step('should go to store page', async ({},testInfo) => {
+        await customTest.step('should go to store page', async () => {
             console.log(testInfo.title);
             await homePage.goToStorePage()
         })
 
-        await customTest.step('should go to product details page', async ({},testInfo) => {
+        await customTest.step('should go to product details page', async () => {
             console.log(testInfo.title);
             await storePage.goToProductDetailsPage(data.productName)
         })
 
-        await customTest.step('should add product to cart', async ({},testInfo) => {
+        await customTest.step('should add product to cart', async () => {
             console.log(testInfo.title);
             await productDetailsPage.addProductToCart(data.numberOfItems)
         })
 
-        await customTest.step('should go to shopping address page', async ({},testInfo) => {
+        await customTest.step('should go to shopping address page', async () => {
             console.log(testInfo.title);
             await viewCartPage.goToShoppingAddressPage();
         })
-        await customTest.step('should add contact details', async ({},testInfo) => {
+        await customTest.step('should add contact details', async () => {
             console.log(testInfo.title);
             await shoppingAddressPage.addContactDetails()
             await shoppingAddressPage.addShippingAddress(data.firstName, data.lastName, data.address, data.city, data.state, data.postalCode)
         })
 
-        await customTest.step('should add payment details', async ({},testInfo) => {
+        await customTest.step('should add payment details', async () => {
             console.log(testInfo.title);
             await paymentDetailsPage.addPaymentDeatils(data.cardNumber, data.nameOnCard, data.expiry, data.cvv)
         })
