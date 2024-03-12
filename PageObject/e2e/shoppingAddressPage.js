da5ca0327429b261e23b2fdff1d810d4e6e72d2d
Noob_Hacker <gauravjais4491@gmail.com>
2024-03-12 16:27:03 +0530

    update first name locator

--- PageObject/e2e/shoppingAddressPage.js
+++ PageObject/e2e/shoppingAddressPage.js
@@ -10,7 +10,7 @@ class ShoppingAddressPage {
     };
 
     get addFirstName() {
-        return this.page.locator(`body > div:nth-child(37) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > main:nth-child(1) > div:nth-child(1) > form:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(5) > div:nth-child(1) > section:nth-child(1) > div:nth-child(1) > div:nth-child(2) > section:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1)> div:first-child>div:nth-child(2)>div:first-child`)
+        return this.page.locator(`section:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1)`)
     }
     get addLastName() {
         return this.page.locator("#TextField1")
