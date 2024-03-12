cfc1ba19a6054c4a91d3f10f2a278f6b26a50ebb
Noob_Hacker <gauravjais4491@gmail.com>
2024-03-12 13:47:58 +0530

    add testinfo for debugging

--- tests/e2e/e2e.spec.js
+++ tests/e2e/e2e.spec.js
@@ -5,33 +5,39 @@ for (const data of e2eData) {
     customTest(`should place an order with product name ' ${data.productName} '`, async ({ homePage, storePage, page, productDetailsPage, viewCartPage, shoppingAddressPage, paymentDetailsPage },testInfo) => {
 
         await customTest.step('should go to store page', async () => {
-            console.log(testInfo.title);
+            console.log("should go to store page");
             await homePage.goToStorePage()
+            console.log("should go to store page1");
         })
 
         await customTest.step('should go to product details page', async () => {
-            console.log(testInfo.title);
+            console.log("should go to product details page");
             await storePage.goToProductDetailsPage(data.productName)
+            console.log("should go to product details page1");
         })
 
         await customTest.step('should add product to cart', async () => {
-            console.log(testInfo.title);
+            console.log("should add product to cart");
             await productDetailsPage.addProductToCart(data.numberOfItems)
+            console.log("should add product to cart1");
         })
 
         await customTest.step('should go to shopping address page', async () => {
-            console.log(testInfo.title);
+            console.log("should go to shopping address page");
             await viewCartPage.goToShoppingAddressPage();
+            console.log("should go to shopping address page1");
         })
         await customTest.step('should add contact details', async () => {
-            console.log(testInfo.title);
+            console.log("should add contact details");
             await shoppingAddressPage.addContactDetails()
             await shoppingAddressPage.addShippingAddress(data.firstName, data.lastName, data.address, data.city, data.state, data.postalCode)
+            console.log("should add contact details1");
         })
 
         await customTest.step('should add payment details', async () => {
-            console.log(testInfo.title);
+            console.log("should add payment details");
             await paymentDetailsPage.addPaymentDeatils(data.cardNumber, data.nameOnCard, data.expiry, data.cvv)
+            console.log("should add payment details1");
         })
     })
 }
